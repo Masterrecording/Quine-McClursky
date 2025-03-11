c9745a300e6e2b95c45036bfa6c1ee2ff2f6f13a
MasterRecording <75507516+Masterrecording@users.noreply.github.com>
2025-03-10 23:46:35 -0600

    Minimal updates

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ cant_de_var = 0
 minterms = []
 
 
-# Introducir todos los mintérminos
-
-# Hacer tuplas de todos los mintérminos que se pueden combinar
-
-# Eliminar los mintérminos repetidos y los que se combinan a sí mismos
-
-# Comparar con los mintérminos originales y ver si hay algúno que no esté en la lista de tuplas. Si hay, almacenarlo como mintérmino primo
-
-# A partir de estas tuplas hacer los términos con X (los llamaremos implicantes)
-
-# Hacer las tuplas de todos los implicantes que se pueden combinar
-
-# Eliminar los implicantes repetidos y los que se combinan a sí mismos
-
-# Comparar con los implicantes originales y ver si hay alguno que no esté en la lista de tuplas. Si hay, almacenarlo como implicante primo
-
-# Repetir este proceso hasta que la longitud de la lista de tuplas sea igual a 0
-
-# Finalmente en la lista de términos primos usar la tabla para hallar a la función
-
 def prime_to_function(prime_terms):
     f = ""
     for term in prime_terms:
